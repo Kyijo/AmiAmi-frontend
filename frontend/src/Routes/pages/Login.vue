fe09ea8ce70147c53a4d1d684c8f6b2638fbbc3d
Kyijo <dominikborufka@seznam.cz>
2023-05-20 19:42:47 +0200

    login,signin,post,page not found done

--- frontend/src/Routes/pages/Login.vue
+++ frontend/src/Routes/pages/Login.vue
@@ -1,5 +1,94 @@
 <template>
-    <h1>
-        Login
-    </h1>
-</template>
+    <div class="flex justify-center items-center b-20">
+      <div class="w-full max-w-sm p-8">
+        <form class="shadow-lg rounded-lg px-8 pt-6 pb-8 mb-4 bg-[#331147]">
+          <div class="mb-4">
+            <label class="block font-bold mb-2 text-white" for="username">
+              Username
+            </label>
+            <input
+              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="username"
+              type="text"
+              placeholder="Enter your username"
+              v-model="username"
+            />
+          </div>
+          <div class="mb-4">
+            <label class="block font-bold mb-2 text-white" for="password">
+              Password
+            </label>
+            <div class="relative">
+              <input
+                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                id="password"
+                :type="showPassword ? 'text' : 'password'"
+                placeholder="Enter your password"
+                v-model="password"
+              />
+              <div class="password-toggle" @click="showPassword = !showPassword">
+                <img
+                  class="password-icon"
+                  src="../../assets/img/logos/password.png"
+                  alt="Show Password"
+                />
+              </div>
+            </div>
+          </div>
+          <div class="flex items-center justify-between">
+            <button
+              class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="button"
+              @click="login">
+              Sign In
+            </button>
+            <a
+              class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+              href="#"
+            >
+              Forgot Password?
+            </a>
+          </div>
+        </form>
+        <p class="text-center text-gray-500 text-xs">
+          &copy;2023 Beep Beep AmiAmi rights.
+        </p>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        username: "",
+        password: "",
+        showPassword: false,
+      };
+    },
+    methods: {
+      login() {
+        // Add your login logic here
+      },
+    },
+  };
+  </script>
+  
+  <style>
+  .password-toggle {
+    position: absolute;
+    top: 40%;
+    right: .5rem;
+    transform: translateY(-50%);
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+  }
+  
+  .password-icon {
+    width: 24px;
+    height: 24px;
+  }
+  
+  </style>
+  
--- frontend/src/Routes/pages/Signin.vue
+++ frontend/src/Routes/pages/Signin.vue
@@ -1,102 +1,128 @@
 <template>
-   <div class="flex justify-center items-center b-20">
-    <div class="w-full max-w-sm form-wrapper">
+  <div class="flex justify-center items-center b-20">
+    <div class="w-full max-w-sm p-8">
       <form class="shadow-lg rounded-lg px-8 pt-6 pb-8 mb-4 bg-[#331147]">
-          <div class="mb-4">
-            <label class="block font-bold mb-2 text-white" for="username">
-              Username
-            </label>
-            <input
-              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="username"
-              type="text"
-              placeholder="Enter your username"
-              v-model="username"
-            />
-          </div>
-          <div class="mb-4">
-            <label class="block font-bold mb-2 text-white" for="email">
-              Email
-            </label>
-            <input
-              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="email"
-              type="email"
-              placeholder="Enter your email"
-              v-model="email"
-            />
-          </div>
-          <div class="mb-4">
-            <label class="block font-bold mb-2 text-white" for="password">
-              Password
-            </label>
+        <div class="mb-4">
+          <label class="block font-bold mb-2 text-white" for="username">
+            Username
+          </label>
+          <input
+            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="username"
+            type="text"
+            placeholder="Enter your username"
+            v-model="username"
+          />
+        </div>
+        <div class="mb-4">
+          <label class="block font-bold mb-2 text-white" for="email">
+            Email
+          </label>
+          <input
+            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="email"
+            type="email"
+            placeholder="Enter your email"
+            v-model="email"
+          />
+        </div>
+        <div class="mb-4">
+          <label class="block font-bold mb-2 text-white" for="password">
+            Password
+          </label>
+          <div class="relative">
             <input
               class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
               id="password"
-              type="password"
+              :type="showPassword ? 'text' : 'password'"
               placeholder="Enter your password"
               v-model="password"
             />
+            <div class="password-toggle" @click="showPassword = !showPassword">
+              <img
+                class="password-icon"
+                src="../../assets/img/logos/password.png"
+                alt="Show Password"
+              />
+            </div>
           </div>
-          <div class="mb-6">
-            <label class="block font-bold mb-2 text-white" for="confirmPassword">
-              Confirm Password
-            </label>
+        </div>
+        <div class="mb-6">
+          <label class="block font-bold mb-2 text-white" for="confirmPassword">
+            Confirm Password
+          </label>
+          <div class="relative">
             <input
               class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
               id="confirmPassword"
-              type="password"
+              :type="showConfirmPassword ? 'text' : 'password'"
               placeholder="Retype your password"
               v-model="confirmPassword"
             />
-            <p class="text-gray-500 text-xs">Your password can go brrrr 💅🏼</p>
-            <p class="text-red-500 text-xs italic" v-if="password !== confirmPassword">
-              Passwords do not match
-            </p>
+            <div class="password-toggle" @click="showConfirmPassword = !showConfirmPassword">
+              <img
+                class="password-icon"
+                src="../../assets/img/logos/password.png"
+                alt="Show Password"
+              />
+            </div>
           </div>
-          <div class="flex items-center justify-between">
-            <button
-              class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-              type="button"
-              @click="signin"
-              :disabled="password !== confirmPassword"
-            >
-              Sign In
-            </button>
-            <a
-              class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
-              href="#"
-            >
-              Forgot Password?
-            </a>
-          </div>
-        </form>
-        <p class="text-center text-gray-500 text-xs">
-          &copy;2023 Beep Beep AmiAmi rights.
-        </p>
-      </div>
+          <p class="text-red-500 text-xs italic" v-if="password !== confirmPassword">
+            Passwords do not match
+          </p>
+        </div>
+        <div class="flex items-center justify-between">
+          <button
+            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="button"
+            @click="signin"
+            :disabled="password !== confirmPassword"
+          >
+            Sign In
+          </button>
+        </div>
+      </form>
+      <p class="text-center text-gray-500 text-xs">
+        &copy;2023 Beep Beep AmiAmi rights.
+      </p>
     </div>
-  </template>
-  
-  <style>
-  .form-wrapper {
-    padding: 2rem;
-  }
-  </style>
+  </div>
+</template>
 
-  <script>
-  export default {
-    data() {
-      return {
-        username: "",
-        email: "",
-        password: "",
-        confirmPassword: "",
-      };
+<script>
+export default {
+  data() {
+    return {
+      username: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+      showPassword: false,
+      showConfirmPassword: false,
+    };
+  },
+  methods: {
+    signin() {
+      // Add your signin logic here
     },
-    methods: {
-      sigin() {
-      },
-    },
-  };
-  </script>
+  },
+};
+</script>
+
+<style>
+.password-toggle {
+  position: absolute;
+  top: 40%;
+  right: .5rem;
+  transform: translateY(-50%);
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+}
+
+.password-icon {
+  width: 24px;
+  height: 24px;
+}
+
+</style>
